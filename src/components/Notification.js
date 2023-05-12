c35180d28f3b3a942cae406d54355f276f689996
YohannHERBET <yohannherbet.pro@gmail.com>
2023-05-12 20:38:13 +0200

    banner 3sec when start, counter task of day, style, color in table

--- src/components/Notification.js
+++
@@ -1,5 +0,0 @@
-export const Notification = () => {
-  const hello = () => { console.log('Hello PERI-G ;)'); }
-  const periG = document.querySelector("h1");
-  periG.addEventListener("click", hello);
-};
