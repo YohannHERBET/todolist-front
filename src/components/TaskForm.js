e0174915a1c5d88f6a86ae048e0e833eb9a5e195
YohannHERBET <yohannherbet.pro@gmail.com>
2023-05-12 22:13:39 +0200

    update task, one form for create and update

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,10 +1,17 @@
 import { createTask } from '../services/APICalls';
 import { refreshTasks } from '../utils/refreshTasks';
+import { updateTask } from '../services/APICalls';
 
 export const TaskForm = () => {
   const form = document.getElementById('task-form');
   const cancelButton = document.getElementById('cancelTask');
   const createButton = document.getElementById('createTask');
+
+  const resetForm = () => {
+    document.getElementById('task-date').value = "";
+    document.getElementById('task-name').value = "";
+    document.getElementById('task-description').value = "";
+  };
 
   // onClick if modal is open don't open it
   createButton.addEventListener('click', () => {
@@ -15,10 +22,14 @@ export const TaskForm = () => {
 
   cancelButton.addEventListener('click', () => {
     form.close();
+    form.removeAttribute('data-task-id');
+    resetForm();
   });
 
   form.addEventListener('submit', async (event) => {
     event.preventDefault();
+    // if we click on the update button, we have a data-task-id
+    const taskIdToUpdate = form.getAttribute('data-task-id');
     const taskDate = document.getElementById('task-date').value;
     const taskName = document.getElementById('task-name').value;
     const taskDescription = document.getElementById('task-description').value;
@@ -30,17 +41,45 @@ export const TaskForm = () => {
       description: taskDescription,
     };
 
-    
-    if (await createTask(newTask)) {
-      // if the request is true, we can clean the inputs 
-      document.getElementById('task-date').value = "";
-      document.getElementById('task-name').value = "";
-      document.getElementById('task-description').value = "";
-      // close the modal
-      form.close();
-      const taskList = document.getElementById('task-list');
-      // refresh the tasklist
-      await refreshTasks(taskList);
+    // if we have a data-task-id
+    if (taskIdToUpdate) {
+      // we update the task
+      if (await updateTask(taskIdToUpdate, newTask)) {
+        // we clean the data attribut
+        form.removeAttribute('data-task-id');
+        resetForm();
+        form.close();
+        // we reload the tasklist
+        const taskList = document.getElementById('task-list');
+        await refreshTasks(taskList);
+      }
+    } else {
+      // if we click on the button for create a task and the form is ok
+      if (await createTask(newTask)) {
+        // if the request is true, we can clean the inputs 
+        resetForm();
+        // close the modal
+        form.close();
+        const taskList = document.getElementById('task-list');
+        // refresh the tasklist
+        await refreshTasks(taskList);
+      }
     }
   });
+};
+
+// method to fill the update task form
+export const updateTaskForm = (task) => {
+  const form = document.getElementById('task-form');
+  const taskDateInput = document.getElementById('task-date');
+  const taskNameInput = document.getElementById('task-name');
+  const taskDescriptionInput = document.getElementById('task-description');
+
+  taskDateInput.value = task.date;
+  taskNameInput.value = task.name;
+  taskDescriptionInput.value = task.description;
+
+  if (!form.hasAttribute('open')) {
+    form.showModal();
+  }
 };
