35fdc748c01436aac3475cca10d5b7b004579e52
YohannHERBET <yohannherbet.pro@gmail.com>
2023-05-12 12:26:40 +0200

    initialization, architecture, compilation, build

---
+++ src/components/TaskTable.js
@@ -0,0 +1,5 @@
+export const TaskTable = () => {
+  const hello = () => { console.log('Hello PERI-G ;)'); }
+  const periG = document.querySelector("h1");
+  periG.addEventListener("click", hello);
+};
---
+++ src/utils/function.js
@@ -0,0 +1,1 @@
+console.log("reusable functions");
